d18a0c3289070c1ef606b3696530985525427ba7
LiYun <616852971@qq.com>
2019-05-07 18:05:28 +0800

    5.7

---
+++ Demos/vue-project/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='lmodal_container'>
+        <div class='lmodal_header'>
+            
+        </div>
+        <div class='lmodal_content'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            width:200
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
